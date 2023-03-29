84d30f87b4d9589f8b64413d94d6e04ee136c753
Ítalo Dórea <100367059+ifdc2308@users.noreply.github.com>
2023-03-29 01:51:58 -0300

    Create Js
    This code adds a PDF file upload field and a button to convert the file to an Excel spreadsheet. When the button is clicked, the PDF file is loaded using the pdf.js library and the PDF pages are extracted as text. The text is then formatted into an array of arrays for the sheet.js library. Once all PDF pages are extracted and formatted into an Excel spreadsheet, the file is saved as a blob in the browser and a download link is created for the user to download the file.

---
+++ script.js
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Converter PDF para Excel</title>
+  </head>
+  <body>
+    <input type="file" id="pdf-file">
+    <button id="convert-button">Converter</button>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.11.338/pdf.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.16.8/xlsx.full.min.js"></script>
+    <script>
+      const pdfFileInput = document.getElementById('pdf-file');
+      const convertButton = document.getElementById('convert-button');
+
+      convertButton.addEventListener('click', () => {
+        const pdfFile = pdfFileInput.files[0];
+
+        // Carrega o arquivo PDF usando a biblioteca pdf.js
+        pdfjsLib.getDocument(URL.createObjectURL(pdfFile)).promise.then(pdf => {
+          const numPages = pdf.numPages;
+
+          // Itera sobre cada página do PDF
+          let excelData = [];
+          for (let pageNum = 1; pageNum <= numPages; pageNum++) {
+            pdf.getPage(pageNum).then(page => {
+              page.getTextContent().then(content => {
+                // Extrai o texto da página e formata em um array de arrays para o sheet.js
+                let pageData = content.items.map(item => item.str.trim());
+                excelData.push(pageData);
+              });
+            });
+          }
+
+          // Aguarda a extração de todas as páginas do PDF antes de converter em um arquivo Excel
+          Promise.all(excelData).then(() => {
+            const workbook = XLSX.utils.book_new();
+            const worksheet = XLSX.utils.aoa_to_sheet(excelData);
+            XLSX.utils.book_append_sheet(workbook, worksheet, 'PDF para Excel');
+            const excelDataArrayBuffer = XLSX.write(workbook, { type: 'array' });
+
+            // Salva o arquivo Excel em um blob e cria um link para download
+            const excelBlob = new Blob([excelDataArrayBuffer], { type: 'application/octet-stream' });
+            const downloadLink = document.createElement('a');
+            downloadLink.href = URL.createObjectURL(excelBlob);
+            downloadLink.download = `${pdfFile.name}.xlsx`;
+            downloadLink.click();
+          });
+        });
+      });
+    </script>
+  </body>
+</html>
