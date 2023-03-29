bb9f75935d82feb87ffa71a0379bdb032b40ef29
Ítalo Dórea <100367059+ifdc2308@users.noreply.github.com>
2023-03-29 17:59:32 -0300

    The addition of content in a PDF
    Update of permanent content addition in the PDF version for Excel download

--- script.js
+++ script.js
@@ -11,6 +11,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.11.338/pdf.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.16.8/xlsx.full.min.js"></script>
     <script>
+      let pdfData = []; // Variável global para armazenar dados do PDF
+
       const pdfFileInput = document.getElementById('pdf-file');
       const convertButton = document.getElementById('convert-button');
 
@@ -22,21 +24,21 @@
           const numPages = pdf.numPages;
 
           // Itera sobre cada página do PDF
-          let excelData = [];
+          let pageDataPromises = [];
           for (let pageNum = 1; pageNum <= numPages; pageNum++) {
-            pdf.getPage(pageNum).then(page => {
-              page.getTextContent().then(content => {
+            pageDataPromises.push(pdf.getPage(pageNum).then(page => {
+              return page.getTextContent().then(content => {
                 // Extrai o texto da página e formata em um array de arrays para o sheet.js
                 let pageData = content.items.map(item => item.str.trim());
-                excelData.push(pageData);
+                pdfData.push(pageData);
               });
-            });
+            }));
           }
 
           // Aguarda a extração de todas as páginas do PDF antes de converter em um arquivo Excel
-          Promise.all(excelData).then(() => {
+          Promise.all(pageDataPromises).then(() => {
             const workbook = XLSX.utils.book_new();
-            const worksheet = XLSX.utils.aoa_to_sheet(excelData);
+            const worksheet = XLSX.utils.aoa_to_sheet(pdfData);
             XLSX.utils.book_append_sheet(workbook, worksheet, 'PDF para Excel');
             const excelDataArrayBuffer = XLSX.write(workbook, { type: 'array' });
 
